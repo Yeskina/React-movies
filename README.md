f0a631719681bcca30dd43453ea5009524c70c70
Yeskina <angelina.yeskina@gmail.com>
2020-09-01 15:35:02 +0300

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-npm start - to run movies app
------
---
+++ precache-manifest.bf7e654c5fd4f3126e53daac4f23d176.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8b072d57043c638c555e3015756083c",
+    "url": "/React-movies/index.html"
+  },
+  {
+    "revision": "7d2ae380a1d95cca7bfa",
+    "url": "/React-movies/static/css/main.639aa5ad.chunk.css"
+  },
+  {
+    "revision": "38db30a341af043024d8",
+    "url": "/React-movies/static/js/2.b301800d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-movies/static/js/2.b301800d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d2ae380a1d95cca7bfa",
+    "url": "/React-movies/static/js/main.1df69ddd.chunk.js"
+  },
+  {
+    "revision": "016fca88da661edc6230",
+    "url": "/React-movies/static/js/runtime-main.3c0a79f8.js"
+  }
+]);
